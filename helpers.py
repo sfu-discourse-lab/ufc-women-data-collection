1e753fed7abfdcfbc563c2b90b2194227ecfb79f
Adam Podoxin <adam.podoxin@gmail.com>
2025-09-16 17:43:27 -0700

    Cleanup article image downloading code

--- helpers.py
+++ helpers.py
@@ -8,3 +8,18 @@ def get_request(url: str):
             "User-Agent": "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.166 Safari/537.36"
         }
     )
+
+
+def get_page_name(page_link: str):
+    page_name = page_link.replace("/", "_")
+
+    if page_name.startswith("_"):
+        page_name = page_name[1:]
+
+    return page_name
+
+
+def get_image_name(image_src: str):
+    return image_src\
+        .split("/")[-1]\
+        .split("?")[0]
